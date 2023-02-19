7bca264e49483d99896d170979e46b7897dc69c2
Dhiraj Kumar Sharma <dhirajksharma.social@gmail.com>
2023-02-19 21:09:25 +0530

    Add blob in dark mode

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faGithubAlt, faLinkedinIn, faMedium} from '@fortawesome/free-brands-svg-icons'
 import Head from "./Head.js"
 import Tab from "./Tab.js"
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(()=>{
+    const blob= document.getElementById("blob");
+    document.body.onpointermove=event=>{
+      const {clientX, clientY}=event;
+      blob.animate({
+        left:`${clientX}px`,
+        top:`${clientY}px`
+      },{duration: 3000, fill: "forwards"})
+    }
+  })
+    
   return (
     <div className='min-h-screen h-max flex flex-col antialiased dark:bg-[#0f1728]'>
+      <div id="blob" className='hidden dark:lg:block bg-amber-100 h-[140px] aspect-square absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 rounded-full blur-3xl pointer-events-none animate-blobrotate'></div>
       <div className='grow'>
       <Head/>
       <Tab/>
